cf4240cbf4f753623a3c15e89fc5de18cec8c756
Evan Williams <m3fawner@gmail.com>
2020-06-23 09:12:43 -0700

    bugfix: Re-enable prettier after fixing vscode

--- frontend/src/components/Page/Page.jsx
+++ frontend/src/components/Page/Page.jsx
@@ -3,18 +3,14 @@ import Container from 'react-bootstrap/Container';
 import Alert from 'react-bootstrap/Alert';
 import Header from './Header.contextual';
 
-export default ({
-  children, isExpired,
-}) => (
-    <>
-      <div className="c-page">
-        <Header />
-        {isExpired && (
-          <Alert variant="warning">
-            Your session has expired. Please login again.
-          </Alert>
-        )}
-        <Container className="c-page-content">{children}</Container>
-      </div>
-    </>
-  );
+export default ({ children, isExpired }) => (
+  <div className="c-page">
+    <Header />
+    {isExpired && (
+      <Alert variant="warning">
+        Your session has expired. Please login again.
+      </Alert>
+    )}
+    <Container className="c-page-content">{children}</Container>
+  </div>
+);
